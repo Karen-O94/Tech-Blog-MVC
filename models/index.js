35f24fdcfc63ed00af0e406e442f938771375f44
Karen <karenyaa@live.co.uk>
2021-03-28 07:55:47 +0100

    added associations between comment and user/post

--- models/index.js
+++ models/index.js
@@ -11,4 +11,22 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+// Comment to User associations - users have many comments and a comment belongs to a user
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Comment to Post associations - blog postss have many comments - a comment belongs to a blog posts
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
 module.exports = { User, Post, Comment };
